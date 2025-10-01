60a8eb2373c9a3b5dfe823e8764883f900165c6d
reomoon <hwanta0981@gmail.com>
2025-10-01 21:16:03 +0900

    v1.06 크롤링 변경

--- dev/crawl_only.py
+++ dev/crawl_only.py
@@ -5,7 +5,12 @@ GitHub Actions용 크롤링 전용 스크립트
 
 import json
 import os
+import sys
 from datetime import datetime
+
+# 상위 디렉토리를 Python 경로에 추가
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from app.crawlers.crawler_manager import CrawlerManager
 from app.crawlers.site_crawlers import PpomppuCrawler, FmkoreaCrawler, BobaeCrawler, DcinsideCrawler
 
